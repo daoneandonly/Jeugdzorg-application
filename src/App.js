abdd6f825b40ccfd0d15e41ef8ee6acbb43ed4b0
Steven Whitton <stevenawhitton@gmail.com>
2018-10-16 12:28:47 +0200

    reworked create-react-app to project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './style.css';
+import Nav from './comp/nav'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+
+      </div>
+    );
+  }
+}
+
+export default App;
